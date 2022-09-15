5b89abc3a20ebb82fd53b9ea74b81d3d01787acb
Berkay-Disli <berkaydisli.work@gmail.com>
2022-09-15 21:01:44 +0300

    Initial Commit

---
+++ RealmPractice/RealmPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmPracticeApp.swift
+//  RealmPractice
+//
+//  Created by Berkay Disli on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct RealmPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
